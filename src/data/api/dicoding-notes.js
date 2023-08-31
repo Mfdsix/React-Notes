20b11f3691a87aca76a06fae154f1b14cd973cc9
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2023-08-31 12:49:21 +0700

    add http interface and request boilerplates

---
+++ src/data/api/dicoding-notes.js
@@ -0,0 +1,134 @@
+import { fetchUrl, fetchUrlWithToken } from "./http";
+
+const requestWrapper = async (request) => {
+    try{
+        return await request();
+    }catch(e){
+        return {
+            error: true,
+            message: e.message
+        }
+    }
+}
+
+const AuthRequest = {
+    async register({
+        name,
+        email,
+        password
+    }){
+        return await requestWrapper(fetchUrl('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        }));
+    },
+
+    async login({
+        email,
+        password
+    }){
+        return await requestWrapper(fetchUrl('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }));
+    },
+
+    async profile(){
+        return await requestWrapper(fetchUrlWithToken('/users/me', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    }
+}
+
+const NoteRequest = {
+    async getAll(){
+        return await requestWrapper(fetchUrlWithToken('/notes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    },
+
+    async getArchived(){
+        return await requestWrapper(fetchUrlWithToken('/notes/archived', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    },
+
+    async getById(noteId){
+        return await requestWrapper(fetchUrlWithToken(`/notes/${noteId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    },
+
+    async create({
+        title,
+        body
+    }){
+        return await requestWrapper(fetchUrlWithToken('/notes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                body
+            })
+        }));
+    },
+
+    async archive(noteId){
+        return await requestWrapper(fetchUrlWithToken(`/notes/${noteId}/archive`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    },
+
+    async unArchive(noteId){
+        return await requestWrapper(fetchUrlWithToken(`/notes/${noteId}/unarchive`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    },
+
+    async remove(noteId){
+        return await requestWrapper(fetchUrlWithToken(`/notes/${noteId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }));
+    }
+}
+
+export {
+    AuthRequest,
+    NoteRequest
+}
